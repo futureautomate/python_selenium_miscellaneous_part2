439cb5ac975f00dcb7c6f56dccb44ba0a475faa2
Tejas <futureautomate@gmail.com>
2020-04-19 02:56:54 +0530

    Add files via upload

---
+++ web_controlv1.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 18 19:08:54 2020
+
+@author: Tejas
+"""
+
+from selenium import webdriver
+import time
+
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+driver = webdriver.Chrome('F:\Channel\webdriver\chromedriver.exe', options=options)
+time.sleep(3)
+
+driver.get('http://google.com')
+
+driver.get('http://facebook.com')
+
+driver.back()
+
+driver.forward()
+
+driver.refresh()
+
+driver.execute_script("window.open('')")
+driver.switch_to_window(driver.window_handles[1])
+
+driver.get('http://google.com')
+driver.switch_to_window(driver.window_handles[0])
+driver.title
+
+driver.close()
